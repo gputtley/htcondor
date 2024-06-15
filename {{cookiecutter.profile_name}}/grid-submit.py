ae4facdf56b2411adce3b71cb823110d9b8af73d
George <[Email>
2024-06-15 10:41:06 +0100

    Fixing bug

--- {{cookiecutter.profile_name}}/grid-submit.py
+++ {{cookiecutter.profile_name}}/grid-submit.py
@@ -16,21 +16,21 @@ UUID = uuid4()  # random UUID
 jobDir = "{{cookiecutter.htcondor_log_dir}}/{}_{}".format(job_properties["jobid"], UUID)
 makedirs(jobDir, exist_ok=True)
 
-sub = htcondor.Submit(
-    {
-        "executable": "/bin/bash",
-        "arguments": jobscript,
-        "max_retries": "5",
-        "log": join(jobDir, "condor.log"),
-        "output": join(jobDir, "condor.out"),
-        "error": join(jobDir, "condor.err"),
-        "getenv": "True",
-    }
-)
+sub_dict = {
+    "executable": "/bin/bash",
+    "arguments": jobscript,
+    "max_retries": "5",
+    "log": join(jobDir, "condor.log"),
+    "output": join(jobDir, "condor.out"),
+    "error": join(jobDir, "condor.err"),
+    "getenv": "True",
+}
 
 if "resources" in job_properties.keys():
     for line in job_properties["resources"]:
-        sub[line.split("=")[0]] = line.split("=")[1]
+        sub_dict[line.split("=")[0]] = line.split("=")[1]
+
+sub = htcondor.Submit(sub_dict)
 
 
 schedd = htcondor.Schedd()
